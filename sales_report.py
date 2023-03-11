8da319f8f7a5556fccf53729b924d23ffb9e5525
Tiela Black-Law <tiela.rose@gmail.com>
2023-03-10 19:56:38 -0800

    Split code into two functions

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,36 @@
-"""Generate sales report showing total melons each salesperson sold."""
+import sys
 
 
-salespeople = []
-melons_sold = []
+def get_sales_by_salesperson(file_path):
+    """Resturn a dictionary of {salesperson: melons_sold}
 
-f = open('sales-report.txt')
+    Arguments:
+        file_path (str) - the path to a sales report log file
 
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
-    salesperson = entries[0]
-    melons = int(entries[2])
+    Return:
+        salesdata (dict)"""
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-        melons_sold[position] += melons
-        
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+    salesdata = {}
+
+    with open(file_path) as file:
+        for line in file:
+            entries = line.rstrip().split('|')
+            salesperson = entries[0]
+            melons = int(entries[2])
+
+            salesdata[salesperson] = salesdata.get(salesperson, 0) + melons
+
+    return salesdata
 
 
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+def print_sales_by_salesperson(dictionary_of_sales):
+    """Print a report of salespeople and the total number of melons they've sold
+
+    Arguments:
+        dictionary_of_sales (dict): {salesperson: melons_sold}"""
+
+    for salesperson, melons_sold in dictionary_of_sales.items():
+        print(f'{salesperson} sold {melons_sold} melons')
+
+
+print_sales_by_salesperson(get_sales_by_salesperson(sys.argv[1]))
